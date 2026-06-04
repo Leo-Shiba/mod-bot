23f5d4b69b119a55f9fd76a75404dc3665b1552a
Leo-Shiba <leoshibaminecraft@gmail.com>
2026-06-04 18:38:42 -0300

    feat: versao inicial do Mod-Bot para WhatsApp

---
+++ core/utils.js
@@ -0,0 +1,11 @@
+// Autoria Leo-Shiba GitHub
+function extrairMencionado(msg) {
+  const mencoes = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid || [];
+  if (mencoes.length) return mencoes[0];
+  const quoted = msg.message?.extendedTextMessage?.contextInfo?.participant;
+  if (quoted) return quoted;
+  return null;
+}
+function jidParaNumero(jid) { return jid.split('@')[0].replace(/\.\d+$/, ''); }
+function formatarHora(ts) { return new Date(ts).toLocaleString('pt-BR', { timeZone: 'America/Belem' }); }
+module.exports = { extrairMencionado, jidParaNumero, formatarHora };
