c725ee6a699d584f3877fdfb3e88e27814efd002
Leo-Shiba <leoshibaminecraft@gmail.com>
2026-06-05 01:05:39 -0300

    feat: versao inicial do Mod-Bot para WhatsApp

---
+++ commands/banir.js
@@ -0,0 +1,3 @@
+// Autoria Leo-Shiba GitHub
+const{extrairMencionado,jidParaNumero}=require('../core/utils');
+module.exports={nome:'baniro',ome:'kl1',aliases:['kick'],descricao:'Remove um membro do grupo.',apenasAdmin:true,apenasGrupo:true,executar:async({sock,msg,jid,db})=>{const alvo=extrairMencionado(msg);if(!alvo)return sock.sendMessage(jid,{text:'⚠️ Mencione quem banir.'});try{await sock.groupParticipantsUpdate(jid,[alvo],'remove');db.resetarAvisos(jid,alvo);await sock.sendMessage(jid,{text:`✅ @${jidParaNumero(alvo)} removido.`,mentions:[alvo]});}catch(e){await sock.sendMessage(jid,{text:'Erro: '+e.message});}}};
